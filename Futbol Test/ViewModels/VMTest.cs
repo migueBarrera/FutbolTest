d84510973b43c57c45729dfe8d84888a067d6149
migueBarrera <mbmdevelop@gmail.com>
2017-02-16 19:51:42 +0100

    empezando a implementar MVVM

---
+++ Futbol Test/ViewModels/VMTest.cs
@@ -0,0 +1,21 @@
+﻿using Futbol_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futbol_Test.ViewModels
+{
+    public class VMTest : clsVMBase
+    {
+        #region Atributos
+        private Pregunta[] test;
+        private Pregunta preginta;
+        private Respuesta respuesta1;
+        private Respuesta respuesta2;
+        private Respuesta respuesta3;
+        private 
+        #endregion
+    }
+}
